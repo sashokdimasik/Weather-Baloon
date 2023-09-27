d2ccb53583b91acd82d4aebb0651751f76d27e36
Alexander Platonov <144921734+sashokdimasik@users.noreply.github.com>
2023-09-27 17:00:33 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -10,18 +10,20 @@
 #include <iomanip>
 #include <math.h>
 
+using namespace std;
+
 int main() {
   double height, temperature;
   
   double liftTime[5] = {0.5, 1, 2, 5, 10};
   
-  std::cout << std::fixed << std::setprecision(2);
+  cout << fixed << setprecision(2);
   
   for (int i = 0; i < 5; i ++) {
     height = 4.87 * sqrt(liftTime[i]);
     if (height < 11) temperature = 15.2 - 6.55 * height;
     else temperature = -56.6 + 0.01 * height;
-    std::cout << "Time = " << liftTime[i] << " Height = " << height << " Temperature = " << temperature << '\n';
+    cout << "Time = " << liftTime[i] << " Height = " << height << " Temperature = " << temperature << '\n';
   }
   
   return 0;
